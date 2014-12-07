3ab199d62b1f2634c034ed734b7215cceea25857
Francis Galiegue <fgaliegue@gmail.com>
2014-12-07 21:11:13 +0100

    Define base implementations of *FileAttributes

--- src/main/java/com/github/fge/filesystem/attributes/attrs/BasicFileAttributesBase.java
+++ src/main/java/com/github/fge/filesystem/attributes/attrs/BasicFileAttributesBase.java
@@ -16,7 +16,7 @@
  * - ASL 2.0: http://www.apache.org/licenses/LICENSE-2.0.txt
  */
 
-package com.github.fge.filesystem.attributes;
+package com.github.fge.filesystem.attributes.attrs;
 
 import java.nio.file.Files;
 import java.nio.file.attribute.BasicFileAttributes;
