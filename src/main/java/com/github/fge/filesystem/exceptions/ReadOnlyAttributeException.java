3f0611eb92b8e950cade37bab505e9b43277dfdc
Francis Galiegue <fgaliegue@gmail.com>
2014-12-07 14:42:51 +0100

    New exception ReadOnlyAttributeException
    This exception extends IOException and is thrown when a given file attribute
    cannot be set.

---
+++ src/main/java/com/github/fge/filesystem/exceptions/ReadOnlyAttributeException.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2014, Francis Galiegue (fgaliegue@gmail.com)
+ *
+ * This software is dual-licensed under:
+ *
+ * - the Lesser General Public License (LGPL) version 3.0 or, at your option, any
+ *   later version;
+ * - the Apache Software License (ASL) version 2.0.
+ *
+ * The text of both licenses is available under the src/resources/ directory of
+ * this project (under the names LGPL-3.0.txt and ASL-2.0.txt respectively).
+ *
+ * Direct link to the sources:
+ *
+ * - LGPL 3.0: https://www.gnu.org/licenses/lgpl-3.0.txt
+ * - ASL 2.0: http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+
+package com.github.fge.filesystem.exceptions;
+
+import java.io.IOException;
+
+public final class ReadOnlyAttributeException
+    extends IOException
+{
+    /**
+     * Constructs an {@code IOException} with {@code null}
+     * as its error detail message.
+     */
+    public ReadOnlyAttributeException()
+    {
+    }
+
+    /**
+     * Constructs an {@code IOException} with the specified detail message.
+     *
+     * @param message The detail message (which is saved for later retrieval
+     * by the {@link #getMessage()} method)
+     */
+    public ReadOnlyAttributeException(final String message)
+    {
+        super(message);
+    }
+
+    /**
+     * Constructs an {@code IOException} with the specified detail message
+     * and cause.
+     * <p> Note that the detail message associated with {@code cause} is
+     * <i>not</i> automatically incorporated into this exception's detail
+     * message.
+     *
+     * @param message The detail message (which is saved for later retrieval
+     * by the {@link #getMessage()} method)
+     * @param cause The cause (which is saved for later retrieval by the
+     * {@link #getCause()} method).  (A null value is permitted,
+     * and indicates that the cause is nonexistent or unknown.)
+     * @since 1.6
+     */
+    public ReadOnlyAttributeException(final String message,
+        final Throwable cause)
+    {
+        super(message, cause);
+    }
+
+    /**
+     * Constructs an {@code IOException} with the specified cause and a
+     * detail message of {@code (cause==null ? null : cause.toString())}
+     * (which typically contains the class and detail message of {@code cause}).
+     * This constructor is useful for IO exceptions that are little more
+     * than wrappers for other throwables.
+     *
+     * @param cause The cause (which is saved for later retrieval by the
+     * {@link #getCause()} method).  (A null value is permitted,
+     * and indicates that the cause is nonexistent or unknown.)
+     * @since 1.6
+     */
+    public ReadOnlyAttributeException(final Throwable cause)
+    {
+        super(cause);
+    }
+}
