fe502712c78694cb5ac2bff707db14e2df0691d3
Francis Galiegue <fgaliegue@gmail.com>
2014-12-07 15:48:30 +0100

    Rename attrs package to attributes

--- src/main/java/com/github/fge/filesystem/attributes/view/FileOwnerAttributeViewBase.java
+++ src/main/java/com/github/fge/filesystem/attributes/view/FileOwnerAttributeViewBase.java
@@ -16,7 +16,7 @@
  * - ASL 2.0: http://www.apache.org/licenses/LICENSE-2.0.txt
  */
 
-package com.github.fge.filesystem.attrs.view;
+package com.github.fge.filesystem.attributes.view;
 
 import java.io.IOException;
 import java.nio.file.attribute.FileOwnerAttributeView;
