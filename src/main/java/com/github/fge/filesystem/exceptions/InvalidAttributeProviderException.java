75010317e09fc6d858f909fbd8ecc648d2ebdd43
Francis Galiegue <fgaliegue@gmail.com>
2014-12-14 13:15:57 +0100

    Javadoc for exceptions

--- src/main/java/com/github/fge/filesystem/exceptions/InvalidAttributeProviderException.java
+++ src/main/java/com/github/fge/filesystem/exceptions/InvalidAttributeProviderException.java
@@ -18,65 +18,33 @@
 
 package com.github.fge.filesystem.exceptions;
 
-import java.security.PrivilegedActionException;
+import com.github.fge.filesystem.attributes.FileAttributesFactory;
+import com.github.fge.filesystem.attributes.provider.FileAttributesProvider;
 
+/**
+ * Exception thrown when an invalid {@link FileAttributesProvider} is found
+ *
+ * @see FileAttributesFactory
+ */
+@SuppressWarnings("UncheckedExceptionClass")
 public final class InvalidAttributeProviderException
     extends IllegalArgumentException
 {
-    /**
-     * Constructs an <code>IllegalArgumentException</code> with no
-     * detail message.
-     */
     public InvalidAttributeProviderException()
     {
     }
 
-    /**
-     * Constructs an <code>IllegalArgumentException</code> with the
-     * specified detail message.
-     *
-     * @param s the detail message.
-     */
     public InvalidAttributeProviderException(final String s)
     {
         super(s);
     }
 
-    /**
-     * Constructs a new exception with the specified detail message and
-     * cause.
-     * <p>Note that the detail message associated with <code>cause</code> is
-     * <i>not</i> automatically incorporated in this exception's detail
-     * message.
-     *
-     * @param message the detail message (which is saved for later retrieval
-     * by the {@link Throwable#getMessage()} method).
-     * @param cause the cause (which is saved for later retrieval by the
-     * {@link Throwable#getCause()} method).  (A <tt>null</tt> value
-     * is permitted, and indicates that the cause is nonexistent or
-     * unknown.)
-     * @since 1.5
-     */
     public InvalidAttributeProviderException(final String message,
         final Throwable cause)
     {
         super(message, cause);
     }
 
-    /**
-     * Constructs a new exception with the specified cause and a detail
-     * message of <tt>(cause==null ? null : cause.toString())</tt> (which
-     * typically contains the class and detail message of <tt>cause</tt>).
-     * This constructor is useful for exceptions that are little more than
-     * wrappers for other throwables (for example, {@link
-     * PrivilegedActionException}).
-     *
-     * @param cause the cause (which is saved for later retrieval by the
-     * {@link Throwable#getCause()} method).  (A <tt>null</tt> value is
-     * permitted, and indicates that the cause is nonexistent or
-     * unknown.)
-     * @since 1.5
-     */
     public InvalidAttributeProviderException(final Throwable cause)
     {
         super(cause);
--- src/main/java/com/github/fge/filesystem/exceptions/UncaughtIOException.java
+++ src/main/java/com/github/fge/filesystem/exceptions/UncaughtIOException.java
@@ -18,82 +18,41 @@
 
 package com.github.fge.filesystem.exceptions;
 
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.spi.FileSystemProvider;
+
+/**
+ * TODO! Due to a design fault, see description
+ *
+ * <p>The problem here is that for the moment, attribute view loading is
+ * <strong>eager</strong> whereas it should be lazy.</p>
+ *
+ * @see FileSystemProvider#getFileAttributeView(Path, Class, LinkOption...)
+ */
+@SuppressWarnings("UncheckedExceptionClass")
 public final class UncaughtIOException
     extends RuntimeException
 {
-    /**
-     * Constructs a new runtime exception with {@code null} as its
-     * detail message.  The cause is not initialized, and may subsequently be
-     * initialized by a call to {@link #initCause}.
-     */
     public UncaughtIOException()
     {
     }
 
-    /**
-     * Constructs a new runtime exception with the specified detail message.
-     * The cause is not initialized, and may subsequently be initialized by a
-     * call to {@link #initCause}.
-     *
-     * @param message the detail message. The detail message is saved for
-     * later retrieval by the {@link #getMessage()} method.
-     */
     public UncaughtIOException(final String message)
     {
         super(message);
     }
 
-    /**
-     * Constructs a new runtime exception with the specified detail message and
-     * cause.  <p>Note that the detail message associated with
-     * {@code cause} is <i>not</i> automatically incorporated in
-     * this runtime exception's detail message.
-     *
-     * @param message the detail message (which is saved for later retrieval
-     * by the {@link #getMessage()} method).
-     * @param cause the cause (which is saved for later retrieval by the
-     * {@link #getCause()} method).  (A <tt>null</tt> value is
-     * permitted, and indicates that the cause is nonexistent or
-     * unknown.)
-     * @since 1.4
-     */
     public UncaughtIOException(final String message, final Throwable cause)
     {
         super(message, cause);
     }
 
-    /**
-     * Constructs a new runtime exception with the specified cause and a
-     * detail message of <tt>(cause==null ? null : cause.toString())</tt>
-     * (which typically contains the class and detail message of
-     * <tt>cause</tt>).  This constructor is useful for runtime exceptions
-     * that are little more than wrappers for other throwables.
-     *
-     * @param cause the cause (which is saved for later retrieval by the
-     * {@link #getCause()} method).  (A <tt>null</tt> value is
-     * permitted, and indicates that the cause is nonexistent or
-     * unknown.)
-     * @since 1.4
-     */
     public UncaughtIOException(final Throwable cause)
     {
         super(cause);
     }
 
-    /**
-     * Constructs a new runtime exception with the specified detail
-     * message, cause, suppression enabled or disabled, and writable
-     * stack trace enabled or disabled.
-     *
-     * @param message the detail message.
-     * @param cause the cause.  (A {@code null} value is permitted,
-     * and indicates that the cause is nonexistent or unknown.)
-     * @param enableSuppression whether or not suppression is enabled
-     * or disabled
-     * @param writableStackTrace whether or not the stack trace should
-     * be writable
-     * @since 1.7
-     */
     public UncaughtIOException(final String message, final Throwable cause,
         final boolean enableSuppression, final boolean writableStackTrace)
     {
